2b782451bfb1388a8ba798bb9fa3e227ec77f258
tuffery <furtuff@gmail.com>
2017-01-06 18:01:50 +0100

    hotfix dayview

--- app/src/main/java/fr/wildcodeschool/haa/waxym/CustomDayAdapter.java
+++ app/src/main/java/fr/wildcodeschool/haa/waxym/CustomDayAdapter.java
@@ -75,8 +75,8 @@ public class CustomDayAdapter extends BaseAdapter {
                 if(this.dayEvents.size() > 0) {
                     for (int i = 0; i < this.dayEvents.size(); i++) {
                         if (this.dayEvents.get(i).getMorning() == 1) {
-                            halfDay.setText(this.dayEvents.get(0).getContractNumber() + " " + this.dayEvents.get(0).getActivity());
-                            gd.setColor(Color.parseColor(this.dayEvents.get(0).getActivityColor()));
+                            halfDay.setText(this.dayEvents.get(i).getContractNumber() + " " + this.dayEvents.get(i).getActivity());
+                            gd.setColor(Color.parseColor(this.dayEvents.get(i).getActivityColor()));
                             gd.setStroke(100, Color.parseColor("#FFFFFF"));
                             halfDay.setBackgroundDrawable(gd);
 
